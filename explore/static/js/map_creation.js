9d8d7ae0269000add271683f5db0b0021cd128b5
Adam Erispaha <aerispaha@gmail.com>
2017-11-04 21:32:34 -0400

    load data from mapbox server

--- explore/static/js/map_creation.js
+++ explore/static/js/map_creation.js
@@ -1,50 +1,29 @@
 
 // Set up the map
 
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWVyaXNwYWhhIiwiYSI6ImNpdWp3ZTUwbDAxMHoyeXNjdDlmcG0zbDcifQ.mjQG7vHfOacyFDzMgxawzw';
 var map = new mapboxgl.Map({
-    style:'mapbox://styles/mapbox/dark-v9',
+    style:'mapbox://styles/aerispaha/cj9lvdi8q1yvs2rn24ytindg0',
     center: [-75.160082, 39.953064],
     zoom: 13,
-    //pitch: 20,
-    //bearing: -17.6,
     container: 'map_wrapper',
 });
 
+var overlay = document.getElementById('map-overlay');
 
 
 map.on('load', function() {
   var geocoder = new MapboxGeocoder({accessToken: mapboxgl.accessToken});
   map.addControl(geocoder);
 
-  //load the data into the map
-  divisions_source = map.addSource('division-data', {'type': 'geojson', 'data': divisions});
-  wards_source = map.addSource('ward-data', {'type': 'geojson','data': wards});
-
-  map.addLayer({
-    "id": "divisions",
-    'type': 'line',
-    "source": "division-data",
-    'paint': {
-      'line-color':'rgba(33,150,243,0.8)',
-      'line-width':0.75,
-    },
-    'layout': {'visibility': 'visible'},
-	});
-  map.addLayer({
-    "id": "divisions-click",
-    'type': 'fill',
-    "source": "division-data",
-    'paint': {
-      'fill-color':'rgba(33,150,243,0)',
-      'fill-outline-color':'rgba(33,150,243,0)',
-    },
-    'layout': {'visibility': 'visible'},
-	});
+  //load interactive layers into the map
   map.addLayer({
     "id": "divisions-hover",
     'type': 'fill',
-    "source": "division-data",
+    "source": "composite",
+    'source-layer':'divisions_cp-3bb6vb', //wards-9rgnox
     'paint': {
       'fill-color':'rgba(33,150,243,0.5)',
       'fill-outline-color':'rgba(33,150,243,1)',
@@ -52,17 +31,6 @@ map.on('load', function() {
     },
     'layout': {'visibility': 'visible'},
     'filter':["==", "DIVISION_NUM", ""],
-	});
-
-  map.addLayer({
-    "id": "wards",
-    'type': 'line',
-    "source": "ward-data",
-    'paint': {
-      'line-color':'rgba(106,165,108,0.9)',
-      'line-width':3,
-    },
-    'layout': {'visibility': 'visible'},
 	});
 
   //Geocoder point layer
@@ -89,46 +57,53 @@ map.on('load', function() {
 
     // When a click event occurs near a polygon, open a popup at the location of
     // the feature, with description HTML from its properties.
-  map.on('click', function (e) {
-    console.log(e.point)
-    var features = map.queryRenderedFeatures(e.point, { layers: ['divisions-click'] });
+  map.on('click','phila-ward-divisions',  function (e) {
+
+    console.log(e)
+    var features = e.features;//= map.queryRenderedFeatures(e.point, { layers: ['phila-ward-divisions', 'phila-wards'] });
     if (!features.length) {
         map.setFilter("divisions-hover", ["==", "DIVISION_NUM", ""]);
         return;
     }
 
-    var feature = features[0];
-    console.log(feature)
+    // var feature = features[0];
+    // console.log(features)
+    for (var i = 0; i < features.length; i++) {
+      feature = features[i]
+      console.log(feature)
 
-    if (feature.layer.id == 'divisions-click'){
-      map.setFilter("divisions-hover", ["==", "DIVISION_NUM", feature.properties.DIVISION_NUM]);
-      ward = "Ward " + feature.properties.DIVISION_NUM.slice(0, 2);
-      div = "Division " + feature.properties.SHORT_DIV_NUM;
-      peeps = JSON.parse(feature.properties.committeepeople);
-      peep_data = [];
-      for (var i = 0; i < peeps.length; i++) {
-        p = peeps[i]
-        com_person_str = p['FULL NAME'];
-        peep_data.push(com_person_str)
-      }
-      peep_data = peep_data.join('<br>')
+      if (feature.layer.id == 'phila-ward-divisions'){
+        map.setFilter("divisions-hover", ["==", "DIVISION_NUM", feature.properties.DIVISION_NUM]);
+        ward = "Ward " + feature.properties.DIVISION_NUM.slice(0, 2);
+        div = "Division " + feature.properties.SHORT_DIV_NUM;
+        peeps = JSON.parse(feature.properties.committeepeople);
+        peep_data = [];
+        for (var i = 0; i < peeps.length; i++) {
+          p = peeps[i]
+          com_person_str = p['FULL NAME'];
+          peep_data.push(com_person_str)
+        }
+        peep_data = peep_data.join('<br>')
 
 
-      console.log(peeps);
-      title = '<h4>' + ward + ' ' + div + '</h4>';
-      var html_message = [title, 'Committee People:', peep_data];
+        console.log(peeps);
+        title = '<h4>' + ward + ' ' + div + '</h4>';
+        var html_message = [title, 'Committee People:', peep_data];
 
-      var popup = new mapboxgl.Popup()
-          .setLngLat(map.unproject(e.point))
-          .setHTML(html_message.join('<br>'))
-          .addTo(map);
-      }
+        var popup = new mapboxgl.Popup()
+            .setLngLat(map.unproject(e.point))
+            .setHTML(html_message.join('<br>'))
+            .addTo(map);
+        }
+    }
+
+
 
     });
     // Use the same approach as above to indicate that the symbols are clickable
     // by changing the cursor style to 'pointer'
     map.on('mousemove', function (e) {
-        var features = map.queryRenderedFeatures(e.point, { layers: ['divisions-click'] });
+        var features = map.queryRenderedFeatures(e.point, { layers: ['phila-ward-divisions'] });
         map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
     });
 
--- explore/views.py
+++ explore/views.py
@@ -6,18 +6,8 @@ import geojson
 # Create your views here.
 def home(request):
 
-    divisions_path = os.path.join(GEO_DATA_DIR, 'divisions_cp.geojson') #r'F:\code\wards\explore\static\geo\divisions.geojson'
-    wards_path = os.path.join(GEO_DATA_DIR, 'wards.geojson')
-
-    with open(divisions_path, 'r') as f:
-        divisions = geojson.loads(f.read())
-
-    with open(wards_path, 'r') as f:
-        wards = geojson.loads(f.read())
-
-
     return render(request, 'explore/home.html', {
             'somevar':'adam',
-            'wards':wards,
-            'divisions':divisions,
+            # 'wards':wards,
+            # 'divisions':divisions,
         })
